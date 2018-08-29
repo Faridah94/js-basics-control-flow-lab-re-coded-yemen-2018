b9a948f11461ff13e25cf732fe6eb9d6cc46e5e9
Farida <faridah.absi@gmail.com>
2018-08-29 11:34:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function scuberGreetingForFeet(ride){
        Message ='This one is on me!';
         return Message;
       }
-  else if ( ride > 2000 ){
+  else if ( ride > 2000  ){
         Message ='I will gladly take your thirty bucks.';
         return Message;
       }
